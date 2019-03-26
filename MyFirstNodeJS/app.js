a10f7ba42f11588284663cd05bfa7ce7cf03c6b3
michael.blessing <michael.blessing@gmx.com>
2019-03-26 14:12:10 +0100

    Test2

--- MyFirstNodeJS/app.js
+++ MyFirstNodeJS/app.js
@@ -1,13 +1,6 @@
-const http = require('http');
-
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.send('HEY DUDE!')
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
